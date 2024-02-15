cf291401a5c67a1c6d9c7cef79dc37eca40feaef
Ayah <88272158+aya200254@users.noreply.github.com>
2024-02-15 13:58:35 +0200

    update

--- app/Http/Controllers/RedditController.php
+++ app/Http/Controllers/RedditController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
@@ -12,7 +11,18 @@ class RedditController extends Controller
         $command = "curl $url";
         $response = exec($command);
 
-        $posts = json_decode($response, true)['data']['children'];
+        if ($response === false) {
+            // Handle error
+            return view('error', ['message' => 'Failed to execute curl command']);
+        }
+
+        $data = json_decode($response, true);
+        if (isset($data['data']['children'])) {
+            $posts = $data['data']['children'];
+        } else {
+            // Handle error
+            return view('error', ['message' => 'Invalid response from Reddit API']);
+        }
 
         return view('reddit', ['posts' => $posts]);
     }
@@ -23,38 +33,19 @@ class RedditController extends Controller
         $command = "curl $url";
         $response = exec($command);
 
-        $posts = json_decode($response, true)['data']['children'];
+        if ($response === false) {
+            // Handle error
+            return view('error', ['message' => 'Failed to execute curl command']);
+        }
+
+        $data = json_decode($response, true);
+        if (isset($data['data']['children'])) {
+            $posts = $data['data']['children'];
+        } else {
+            // Handle error
+            return view('error', ['message' => 'Invalid response from Reddit API']);
+        }
 
         return view('reddit', ['posts' => $posts]);
     }
-
-    public function getRisingPosts()
-{
-    $url = 'https://www.reddit.com/r/FlutterDev/rising.json';
-    $command = "curl $url";
-    $response = exec($command);
-
-    if ($response === false) {
-
-        return view('error', ['message' => 'Error executing curl command']);
-    }
-
-
-    $data = json_decode($response, true);
-
-
-    if (!isset($data['data']['children'])) {
-
-        return view('error', ['message' => 'Invalid JSON response']);
-    }
-
-    $posts = $data['data']['children'];
-
-   
-    return view('reddit', ['posts' => $posts]);
 }
-
-}
-
-
-
--- resources/views/reddit.blade.php
+++ resources/views/reddit.blade.php
@@ -2,13 +2,36 @@
 <html>
 <head>
     <title>Reddit Posts</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h1>Reddit Posts</h1>
-    <ul>
-        @foreach ($posts as $post)
-            <li>{{ $post['data']['title'] }}</li>
-        @endforeach
-    </ul>
+    <div class="container">
+        <h1 class="mt-5">Reddit Posts</h1>
+        <table class="table mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Thumbnail</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Author</th>
+                    <th scope="col">Created</th>
+                    <th scope="col">Actions</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($posts as $post)
+                    <tr>
+                        <td><img src="{{ $post['data']['thumbnail'] }}" class="img-fluid" alt="Thumbnail"></td>
+                        <td>{{ $post['data']['title'] }}</td>
+                        <td>{{ $post['data']['author'] }}</td>
+                        <td>{{ $post['data']['created_utc'] }}</td>
+                        <td><a href="{{ $post['data']['url'] }}" class="btn btn-primary btn-sm" target="_blank">View Post</a></td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
