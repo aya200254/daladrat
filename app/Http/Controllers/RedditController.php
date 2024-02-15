620d45bf9aba600391d2706e27afb381d89f8863
Ayah <88272158+aya200254@users.noreply.github.com>
2024-02-15 16:24:08 +0200

    updates

--- app/Http/Controllers/RedditController.php
+++ app/Http/Controllers/RedditController.php
@@ -48,4 +48,26 @@ class RedditController extends Controller
 
         return view('reddit', ['posts' => $posts]);
     }
+
+    public function getRisingPosts()
+{
+    $url = 'https://www.reddit.com/r/FlutterDev/rising.json';
+    $command = "curl $url";
+    $response = exec($command);
+
+    if ($response === false) {
+        // Handle error
+        return view('error', ['message' => 'Failed to execute curl command']);
+    }
+
+    $data = json_decode($response, true);
+    if (isset($data['data']['children'])) {
+        $posts = $data['data']['children'];
+    } else {
+        // Handle error
+        return view('error', ['message' => 'Invalid response from Reddit API']);
+    }
+
+    return view('reddit', ['posts' => $posts]);
+}
 }
